913fe197c64c9f289156a1faba0819e4b0534700
Nitesh050 <mniteshpandey.005@gmail.com>
2026-02-10 18:34:50 +0530

    asd

--- Frontend/my-app/src/main.jsx
+++ Frontend/my-app/src/main.jsx
@@ -1,14 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import { HashRouter } from "react-router-dom"; // ✅ correct import
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>   {/* ✅ wrap App inside */}
-      <App />
-    </HashRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
